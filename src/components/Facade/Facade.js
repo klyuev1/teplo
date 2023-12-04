a0593b863292d92b0fb7a06d750722af80bee753
Oleg Klyuev <olegkluev1507@gmail.com>
2023-12-05 00:19:29 +0500

    feat: Верстка фасадов

--- src/components/Facade/Facade.js
+++ src/components/Facade/Facade.js
@@ -1,88 +1,13 @@
 import React from 'react';
-import FacadeSample from '../../images/image 38.png'
 
-function Facade() {
+function Facade(props) {
   return (
-    <>
     <article className='element'>
       <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
+        <img className='element__photo' src={props.link} alt='facade'/>
+        <h3 className='element__number'>{props.name}</h3>
       </button>
     </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    <article className='element'>
-      <button className='element__button' type='button'>
-        <img className='element__photo' src={FacadeSample}/>
-        <h3 className='element__number'> Номер 6</h3>
-      </button>
-    </article>
-
-    </>
-    
   );
 }
 
---
+++ src/utils/facade-data.js
@@ -0,0 +1,38 @@
+const facadesData = [
+  {
+      name: 'facade 1',
+      link: 'https://i.pinimg.com/236x/83/eb/c0/83ebc04b8547558a9f5d55a069363883.jpg',
+      height: 3400,
+      width: 2600,
+      areaWall: 1.2,
+  },
+  {
+    name: 'facade 2',
+    link: 'https://i.pinimg.com/236x/69/0e/5c/690e5c13bb92d737f287b5aa4b8689d9.jpg',
+    height: 3400,
+    width: 2600,
+    areaWall: 1.4,
+  },
+  {
+    name: 'facade 3',
+    link: 'https://i.pinimg.com/236x/83/eb/c0/83ebc04b8547558a9f5d55a069363883.jpg',
+    height: 3400,
+    width: 2600,
+    areaWall: 1.0,
+  },
+  {
+    name: 'facade 4',
+    link: 'https://i.pinimg.com/236x/83/eb/c0/83ebc04b8547558a9f5d55a069363883.jpg',
+    height: 3400,
+    width: 5000,
+    areaWall: 1.2,
+  },
+  {
+    name: 'facade 5',
+    link: 'https://i.pinimg.com/236x/83/eb/c0/83ebc04b8547558a9f5d55a069363883.jpg',
+    height: 3400,
+    width: 5000,
+    areaWall: 0.9,
+  },
+]
+export default facadesData;
