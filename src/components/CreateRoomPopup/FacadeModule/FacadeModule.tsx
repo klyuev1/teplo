8275c7af387ad8616f66976d568a2d23fe556eab
Oleg Klyuev <olegkluev1507@gmail.com>
2024-03-04 21:51:45 +0500

    feat: Исправлен InfoToolTip и src фасадов

--- src/components/CreateRoomPopup/FacadeModule/FacadeModule.tsx
+++ src/components/CreateRoomPopup/FacadeModule/FacadeModule.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { IFacadeModule } from '../../../models/props';
+import { BASE_URL } from '../../../utils/constants';
 
 function FacadeModule({facade, setHeight, setWidth, setAreaWall, setAreaWindow, setNumberFacade}: IFacadeModule) {
   
-  // setHeight(facade.height);
   const handleRadioChange = () => {
-    setHeight((facade.height)/1000); // Убрать этот кусок кода после редактирования БЭКа
-    setWidth((facade.width)/1000); // Убрать этот кусок кода после редактирования БЭКа
+    setHeight((facade.height)/1000);
+    setWidth((facade.width)/1000);
     setAreaWall(facade.areaWall);
     setAreaWindow(facade.areaWindow);
     setNumberFacade(facade.name);
@@ -17,7 +17,7 @@ function FacadeModule({facade, setHeight, setWidth, setAreaWall, setAreaWindow, 
     <article className='popup-facade-room'>
       <input type='radio' name="facade" className='popup-facade-room__input' onChange={handleRadioChange} required />
       <div className='popup-facade-room__card'>
-        <img src={facade.link} className='popup-facade-room__img'  alt='facade'/>
+        <img src={`${BASE_URL}/${facade.link}`} className='popup-facade-room__img'  alt='facade'/>
         <h3 className='popup-facade-room__title'>{facade.name}</h3>
       </div >
     </article>
